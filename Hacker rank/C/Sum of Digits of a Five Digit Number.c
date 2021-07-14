3913b47a538133324ce0ccaf16e8fa1bc4797ba7
Sanjay  N T <sanjaysanju12120@gmail.com>
2021-07-14 22:37:11 +0530

    Day 9

---
+++ Hacker rank/C/Sum of Digits of a Five Digit Number.c
@@ -0,0 +1,49 @@
+Objective
+
+The modulo operator, %, returns the remainder of a division. For example, 4 % 3 = 1 and 12 % 10 = 2. The ordinary division operator, /, returns a truncated integer value when performed on integers. For example, 5 / 3 = 1. To get the last digit of a number in base 10, use  as the modulo divisor.
+
+Task
+
+Given a five digit integer, print the sum of its digits.
+
+Input Format
+
+The input contains a single five digit number, .
+
+Constraints
+
+
+Output Format
+
+Print the sum of the digits of the five digit number.
+
+Sample Input 0
+
+10564
+Sample Output 0
+
+16
+_________________________________________________________________________________________________
+Sum of Digits of a Five Digit Number
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+	
+    int n,sum =0,j=0;
+    scanf("%d", &n);
+    //Complete the code to calculate the sum of the five digits on n.
+    for(int i=0;i<5;i++)
+    { if(n!= 0)
+    {
+        j = n%10;
+        sum +=j;
+        n =n/10;
+
+    }
+    }
+    printf("%d",sum);
+    return 0;
+}
