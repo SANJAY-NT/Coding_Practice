ddca10a230aecd62416301e0f0f654b13e74bf98
Sanjay  N T <sanjaysanju12120@gmail.com>
2021-08-20 22:36:30 +0530

    Day 37

--- Hacker rank/Python/Symmetric Difference.py
+++ Hacker rank/Python/Symmetric Difference.py
@@ -1,86 +1,128 @@
-An OrderedDict is a dictionary that remembers the order of the keys that were inserted first. If a new entry overwrites an existing entry, the original insertion position is left unchanged.
+Objective
+Today, we're learning about a new data type: sets.
 
-Example
+Concept
 
-Code
+If the inputs are given on one line separated by a character (the delimiter), use split() to get the separate values in the form of a list. The delimiter is space (ascii 32) by default. To specify that comma is the delimiter, use string.split(','). For this challenge, and in general on HackerRank, space will be the delimiter.
 
->>> from collections import OrderedDict
->>> 
->>> ordinary_dictionary = {}
->>> ordinary_dictionary['a'] = 1
->>> ordinary_dictionary['b'] = 2
->>> ordinary_dictionary['c'] = 3
->>> ordinary_dictionary['d'] = 4
->>> ordinary_dictionary['e'] = 5
->>> 
->>> print ordinary_dictionary
-{'a': 1, 'c': 3, 'b': 2, 'e': 5, 'd': 4}
->>> 
->>> ordered_dictionary = OrderedDict()
->>> ordered_dictionary['a'] = 1
->>> ordered_dictionary['b'] = 2
->>> ordered_dictionary['c'] = 3
->>> ordered_dictionary['d'] = 4
->>> ordered_dictionary['e'] = 5
->>> 
->>> print ordered_dictionary
-OrderedDict([('a', 1), ('b', 2), ('c', 3), ('d', 4), ('e', 5)])
+Usage:
+
+>> a = raw_input()
+5 4 3 2
+>> lis = a.split()
+>> print (lis)
+['5', '4', '3', '2']
+If the list values are all integer types, use the map() method to convert all the strings to integers.
+
+>> newlis = list(map(int, lis))
+>> print (newlis)
+[5, 4, 3, 2]
+Sets are an unordered collection of unique values. A single set contains values of any immutable data type.
+
+CREATING SETS
+
+>> myset = {1, 2} # Directly assigning values to a set
+>> myset = set()  # Initializing a set
+>> myset = set(['a', 'b']) # Creating a set from a list
+>> myset
+{'a', 'b'}
+
+
+MODIFYING SETS
+
+Using the add() function:
+
+>> myset.add('c')
+>> myset
+{'a', 'c', 'b'}
+>> myset.add('a') # As 'a' already exists in the set, nothing happens
+>> myset.add((5, 4))
+>> myset
+{'a', 'c', 'b', (5, 4)}
+
+Using the update() function:
+
+>> myset.update([1, 2, 3, 4]) # update() only works for iterable objects
+>> myset
+{'a', 1, 'c', 'b', 4, 2, (5, 4), 3}
+>> myset.update({1, 7, 8})
+>> myset
+{'a', 1, 'c', 'b', 4, 7, 8, 2, (5, 4), 3}
+>> myset.update({1, 6}, [5, 13])
+>> myset
+{'a', 1, 'c', 'b', 4, 5, 6, 7, 8, 2, (5, 4), 13, 3}
+
+REMOVING ITEMS
+
+Both the discard() and remove() functions take a single value as an argument and removes that value from the set. If that value is not present, discard() does nothing, but remove() will raise a KeyError exception.
+
+>> myset.discard(10)
+>> myset
+{'a', 1, 'c', 'b', 4, 5, 7, 8, 2, 12, (5, 4), 13, 11, 3}
+>> myset.remove(13)
+>> myset
+{'a', 1, 'c', 'b', 4, 5, 7, 8, 2, 12, (5, 4), 11, 3}
+
+
+COMMON SET OPERATIONS Using union(), intersection() and difference() functions.
+
+>> a = {2, 4, 5, 9}
+>> b = {2, 4, 11, 12}
+>> a.union(b) # Values which exist in a or b
+{2, 4, 5, 9, 11, 12}
+>> a.intersection(b) # Values which exist in a and b
+{2, 4}
+>> a.difference(b) # Values which exist in a but not in b
+{9, 5}
+
+The union() and intersection() functions are symmetric methods:
+
+>> a.union(b) == b.union(a)
+True
+>> a.intersection(b) == b.intersection(a)
+True
+>> a.difference(b) == b.difference(a)
+False
+These other built-in data structures in Python are also useful.
+
 Task
-
-You are the manager of a supermarket.
-You have a list of  items together with their prices that consumers bought on a particular day.
-Your task is to print each item_name and net_price in order of its first occurrence.
-
-item_name = Name of the item.
-net_price = Quantity of the item sold multiplied by the price of each item.
+Given  sets of integers,  and , print their symmetric difference in ascending order. The term symmetric difference indicates those values that exist in either  or  but do not exist in both.
 
 Input Format
 
-The first line contains the number of items, .
-The next  lines contains the item's name and price, separated by a space.
-
-Constraints
-
+The first line of input contains an integer, .
+The second line contains  space-separated integers.
+The third line contains an integer, .
+The fourth line contains  space-separated integers.
 
 Output Format
 
-Print the item_name and net_price in order of its first occurrence.
+Output the symmetric difference integers in ascending order, one per line.
 
 Sample Input
 
-9
-BANANA FRIES 12
-POTATO CHIPS 30
-APPLE JUICE 10
-CANDY 5
-APPLE JUICE 10
-CANDY 5
-CANDY 5
-CANDY 5
-POTATO CHIPS 30
+STDIN       Function
+-----       --------
+4           set a size M = 4
+2 4 5 9     a = {2, 4, 5, 9}
+4           set b size N = 4
+2 4 11 12   b = {2, 4, 11, 12}
 Sample Output
 
-BANANA FRIES 12
-POTATO CHIPS 60
-APPLE JUICE 20
-CANDY 20
-Explanation
+5
+9
+11
+12
 
-BANANA FRIES: Quantity bought: , Price: 
-Net Price: 
-POTATO CHIPS: Quantity bought: , Price: 
-Net Price: 
-APPLE JUICE: Quantity bought: , Price: 
-Net Price: 
-CANDY: Quantity bought: , Price: 
-Net Price: 
-  ____________________________________________________________
-  # Enter your code here. Read input from STDIN. Print output to STDOUT
-from collections import OrderedDict
-d = OrderedDict()
-for _ in range(int(input())):
-    item, space, quantity = input().rpartition(' ')
-    d[item] = d.get(item, 0) + int(quantity)
-for item, quantity in d.items():
-    print(item, quantity)
++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+n = int(input())
+s = set(map(int,input().split()))
 
+m = int(input())
+s= (s.symmetric_difference(set(map(int,input().split()))))
+i = len(s)
+l = list(s)
+l.sort()
+for j in range(i):
+    print(l[j])
